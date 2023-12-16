3b5d2c1f4fe055bdd7ee101b805eecf20d7ec7a2
JeongWoongGyo <wg.jeong@springworks.co.kr>
2023-12-16 22:39:28 +0900

    동시성 제어 - synchronized - JPA 프로젝트 세팅

---
+++ concurrency-synchronized-jpa/src/main/java/org/example/concurrencysynchronizedjpa/ConcurrencySynchronizedJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.concurrencysynchronizedjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencySynchronizedJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencySynchronizedJpaApplication.class, args);
+    }
+
+}
---
+++ concurrency-synchronized-jpa/src/test/java/org/example/concurrencysynchronizedjpa/ConcurrencySynchronizedJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.concurrencysynchronizedjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencySynchronizedJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
