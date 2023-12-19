1c2ed8ddc4fe9bb86f1c744fba30b84106341689
JeongWoongGyo <wg.jeong@springworks.co.kr>
2023-12-19 20:32:17 +0900

    feat: JSP Exception 프로젝트 세팅

---
+++ jsp-exception/src/main/java/org/example/jspexception/JspExceptionApplication.java
@@ -0,0 +1,13 @@
+package org.example.jspexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspExceptionApplication.class, args);
+    }
+
+}
