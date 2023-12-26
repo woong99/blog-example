f1587b0e09572c6e33c64272bd5a628fdb564b7e
JeongWoongGyo <wg.jeong@springworks.co.kr>
2023-12-26 20:44:45 +0900

    feat: model vs modelMap vs modelAndView 프로젝트 세팅

---
+++ model/src/test/java/org/example/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
